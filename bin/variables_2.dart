3370ee56ecda2c266d0098bae365c9ca434f0fe4
Guillermo Ojeda <gro1413@hotmail.com>
2023-04-10 12:08:07 -0300

    first commit

---
+++ bin/variables_2.dart
@@ -0,0 +1,24 @@
+void main(List<String> arguments) {
+  
+  String nombre;
+  var name = "Sirius";
+
+  int edad = 20;
+
+  double estatura = 1.75;
+
+  bool sadssdf = false;
+
+  nombre = "Sirius";
+
+  final pi = 3.14; 
+  
+  const mesesDelAnio = 12;
+
+  /* Tanto final como const se usan para declarar variables que no pueden ser cambiadas más adelante en el código.
+
+   Pero hay una diferencia importante entre ambos, y es que final permite que la variable sea asignada en tiempo de ejecución,
+   mientras que const requiere que el valor sea conocido en tiempo de compilación (el compilador necesita saber de antemano
+   que hay dentro de const antes de ejecutar el programa, mientras que final puede ser asignado una vez ejecutado el programa). 
+  */
+}
